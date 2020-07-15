bfb07e2231cdf380945a84b359e146102907be7c
balbino aylagas <balbino_a@hotmail.com>
2020-07-14 19:00:21 -0600

    added session code

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from MethodUtil import MethodUtil
 
 
 app = Flask(__name__)
+app.secret_key = "python es bien chivo"
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    if "username" in session:
+        return f"Logged in as {session['username']}"
+    return "You are not logged in"
+
+
+#                    methods=["GET", "POST"]
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        print(MethodUtil.list_ALL())
+        session["username"] = "balbino"
+    return "Now you are log in"
+
+
+@app.route("/logout", methods=MethodUtil.list_ALL())
+def logout():
+    session.pop("username", None)
+    return "Now you are logged out"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     # app.run(debug=True)
